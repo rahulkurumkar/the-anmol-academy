d37de7124ce9de13bde1906a98523fb224aeb488
Prajkta Kurumkar <prajbhapkar@gmail.com>
2020-02-27 12:31:33 +0530

    corrected

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Stack, Text, Link, FontWeights } from "office-ui-fabric-react";
 import Header from "./components/Header/Header";
 import MainPage from "./components/MainPage/MainPage";
 import FooterPage from "./components/FooterPage/FooterPage";
-
+import './App.css'
 const boldStyle = { root: { fontWeight: FontWeights.semibold } };
 
 export const App: React.FunctionComponent = () => {
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,28 +1,53 @@
 import * as React from "react";
-import FooterPage from "../FooterPage/FooterPage";
+//import FooterPage from "../FooterPage/FooterPage";
 import NavBar from "../NavBar/NavBar";
+import Courses from "./Courses";
+//import { Button } from "react-bootstrap";
+
 export interface IMainPageProps {}
 
 export default class MainPage extends React.Component<IMainPageProps> {
   public render() {
     return (
-      <>
+      <div
+        style={{
+          width: "100%",
+          height: "100%"
+        }}
+      >
+        <div>
+          <NavBar />
+        </div>
+
         <div
           style={{
             width: "100%",
             height: "100%"
           }}
         >
-          <div
-            style={{
-              width: "100%",
-              height: "100%"
-            }}
-          >
-            <NavBar />
+          <div className="row">
+            <div className="col-md-12">
+              <div className="courseHeading">
+                <h2>Our Courses</h2>
+              </div>
+            </div>
           </div>
-        </div>{" "}
-      </>
+          <div className="courses">
+            <div className="courseName">
+              <Courses />
+            </div>
+            <div className="courseName">
+              <Courses />
+            </div>
+            <div className="courseName">
+              <Courses />
+            </div>
+            <div className="courseName">
+              <Courses />
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
