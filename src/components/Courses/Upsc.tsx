986df91e1cab3f63097de9202ae3dc425bdea4c6
PrajktaKurumkar <55708351+PrajktaKurumkar@users.noreply.github.com>
2020-03-06 13:26:28 +0530

    added CSS animations (#3)
    * added CSS animations
    
    * changes done

--- src/components/Courses/Upsc.tsx
+++ src/components/Courses/Upsc.tsx
@@ -1,10 +1,119 @@
 import React from "react";
 
-
 export default class Upsc extends React.Component {
   public render() {
     return (
       <div>
+        <div
+          style={{
+            paddingLeft: "50px",
+            paddingRight: "50px",
+            margin: "auto 0"
+          }}
+        >
           <h1>UPSC</h1>
-      </div>)
-    }};
+          <p>
+            There is a dearth of awareness about UPSC as a strong career option,
+            not only amongst the rural population but also amongst the well
+            educated, urban middle class. When we utter the word ‘career’,
+            people, especially anxious parents and their wards think of
+            Engineering (with preference attached to software engineering) ,
+            Medical, Management, Architecture so on and so forth. We generally
+            read in the interviews of successful, meritorious students from 10th
+            or 12th Boards that they would like to become engineers, doctors or
+            these days – MBAs. However, very few of them as well as their
+            parents are aware about UPSC or Union civil services as a robust
+            career option, which not only can give name, fame as well as a
+            secure, stable and enriching career path but also immense inner
+            satisfaction of serving the society in true sense of the term. Union
+            Public Service Commission is an autonomous constitutional body which
+            recruits personnel for different Union Services (except Armed
+            Forces). Civil Service is a crucial institution, which is
+            responsible for implementing the laws made by the legislature as
+            well as the policies formulated by the government. This is the
+            higher class among the public administration, which includes
+            domestic as well as foreign affairs. (However, it does not include
+            judiciary or Armed forces). The Indian Civil Services are
+            categorized as Group A, Group B, Group C and Group D services.
+            Through a rigorous examination procedure, UPSC recruits personnel
+            for about 24 services. The list is given below:
+          </p>
+          <h3>
+            <strong>All India Services</strong>
+          </h3>
+          <ol>
+            <li>Indian Administrative Service</li>
+            <li>Indian Police Service</li>
+          </ol>
+          <h3>
+            <strong>Group A Services</strong>
+          </h3>
+          <ol>
+            <li>Indian Foreign Service</li>
+            <li>Indian P &amp; T Accounts &amp; Finance Service</li>
+            <li>Indian Audit and Accounts Service</li>
+            <li>Indian Revenue Service (Customs and Central Excise)</li>
+            <li>Indian Defence Accounts Service</li>
+            <li>Indian Revenue Service (I.T.)</li>
+            <li>
+              Indian Ordnance Factories Service (Assistant Works Manager,
+              Administration)
+            </li>
+            <li>Indian Postal Service</li>
+            <li>Indian Civil Accounts Service</li>
+            <li>Indian Railway Traffic Service</li>
+            <li>Indian Railway Accounts Service</li>
+            <li>Indian Railway Personnel Service</li>
+            <li>
+              Post of Assistant Security Commissioner in Railway Protection
+              Force
+            </li>
+            <li>Indian Defence Estates Service</li>
+            <li>Indian Information Service (Junior Grade)</li>
+            <li>Indian Trade Service, Group ‘A’ (Gr. III)</li>
+            <li>Indian Corporate Law Service</li>
+          </ol>
+          <h3>
+            <strong>Group B Services</strong>
+          </h3>
+          <ol>
+            <li>
+              Armed Forces Headquarters Civil Service (Section Officer’s Grade)
+            </li>
+            <li>
+              Delhi, Andaman &amp; Nicobar Islands, Lakshadweep, Daman &amp; Diu
+              and Dadra &amp; Nagar Haveli Civil Service
+            </li>
+            <li>
+              Delhi, Andaman &amp; Nicobar Islands, Lakshadweep, Daman &amp; Diu
+              and Dadra &amp; Nagar Haveli Police Service
+            </li>
+            <li>Pondicherry Civil Service</li>
+            <li>Pondicherry Police Service</li>
+          </ol>
+          <p>
+            The candidates which are selected by the UPSC are recruited by the
+            Government of India for these various services depending on their
+            rank. The selected candidates are given training of their respective
+            services and then they join their service in any of the state/zone
+            in India, which is assigned to them.
+          </p>
+          <h3>
+            <strong>Why UPSC is a desirable career option?</strong>
+          </h3>
+          <p>
+            UPSC has all the ingredients, which are essential to make it a sound
+            career option, namely stability of tenure, attractive salary and
+            social status. But more than that the powers which are bestowed upon
+            the person occupying these posts and the opportunity to utilize
+            these powers for the benefit of the ‘Public’ gives it an edge over
+            the other career options. Because, here in UPSC, your aim doesn’t
+            get confined to ‘private gain’ but you can serve ‘public purpose’
+            through your career. This sense of fulfilment is the most important
+            thing that makes it a coveted career.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
