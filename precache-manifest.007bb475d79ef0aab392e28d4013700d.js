dc6757bbe8a1ebbe4dfa1c2c675def83514f48fb
Rahul Kurumkar <8025925+rahulkurumkar@users.noreply.github.com>
2020-03-10 13:10:11 +0530

    Updates

---
+++ precache-manifest.007bb475d79ef0aab392e28d4013700d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3d0a7a58a7d998203d5dea4ca4e529",
+    "url": "/rahulkurumkar/anmol-academy/index.html"
+  },
+  {
+    "revision": "286406613ad7823cca8a",
+    "url": "/rahulkurumkar/anmol-academy/static/css/2.1d7b93f6.chunk.css"
+  },
+  {
+    "revision": "adb990ecb8da01e3a965",
+    "url": "/rahulkurumkar/anmol-academy/static/css/main.5280ae0d.chunk.css"
+  },
+  {
+    "revision": "286406613ad7823cca8a",
+    "url": "/rahulkurumkar/anmol-academy/static/js/2.8935fa2a.chunk.js"
+  },
+  {
+    "revision": "91bcc555fc9d257ab7cac84356530039",
+    "url": "/rahulkurumkar/anmol-academy/static/js/2.8935fa2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb990ecb8da01e3a965",
+    "url": "/rahulkurumkar/anmol-academy/static/js/main.8a6fe4ce.chunk.js"
+  },
+  {
+    "revision": "0532dc890fdc5140c2be",
+    "url": "/rahulkurumkar/anmol-academy/static/js/runtime-main.938dc684.js"
+  },
+  {
+    "revision": "918cd5ef860e345abf629d821e360aff",
+    "url": "/rahulkurumkar/anmol-academy/static/media/IMG_8847.918cd5ef.JPG"
+  },
+  {
+    "revision": "9791b3a3f8e1a43845aa98d5c19ccee0",
+    "url": "/rahulkurumkar/anmol-academy/static/media/P 1 .9791b3a3.png"
+  },
+  {
+    "revision": "cc5b46c5a5cad662666033d7e9c1762b",
+    "url": "/rahulkurumkar/anmol-academy/static/media/banner1.cc5b46c5.png"
+  },
+  {
+    "revision": "6e533667a32bd9d7d6a831accbf3878f",
+    "url": "/rahulkurumkar/anmol-academy/static/media/banner3.6e533667.jpg"
+  }
+]);
